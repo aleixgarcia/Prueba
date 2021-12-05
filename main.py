8b915210f211d8196225fd38f41ab7bc56a47058
aleixgarcia <agarcia1@institutmvm.cat>
2021-12-05 14:36:00 +0100

    VALIDACION -Dissenya un algorisme que demani introduir N notes de 0 a 10 (N és el nombre de notes i s‘ha d’indicar per teclat), calculi la mitjana d'aprovats i la mitjana dels suspesos i indiqui la quantitat d'aprovats i suspesos.

--- main.py
+++ main.py
@@ -3,9 +3,7 @@
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
 def main():
-
     sumaA=0
     sumaS=0
     auxA=0
@@ -15,8 +13,11 @@ def main():
     reg=int(input("Introdueix el num de registres de notes:"))
 
     for x in range(reg):
-        notes.append(int(input("Introdueix la nota de l'alumne:")))
-        if notes[x]>=4:
+        nota=int(input("Introdueix la nota de l'alumne:"))
+        if nota<0 or nota>10:
+            nota=int(input("Introdueix la nota menor de 10 i major de 0:"))
+        notes.append(nota)
+        if notes[x]>4:
             sumaA=notes[x]+sumaA
             auxA=auxA+1
         else:
@@ -24,12 +25,13 @@ def main():
             auxS=auxS+1
     sumaA = sumaA / auxA
     sumaS = sumaS / auxS
-    print("***************************************")
-    print("Les notes dels alumnes son: ")
+    print("*************************************************")
+    print("Les notes dels alumnes son: {", end="")
     for x in range(reg-1):
         print(notes[x], end=",")
-    print(notes[-1],end="")
-    print("\n***************************************")
+    else:
+        print(notes[-1],end="}")
+    print("\n*************************************************")
 
     print("La mitjana dels aprobats es: ",sumaA)
     print("La mitjana dels suspesos es: ", sumaS)
