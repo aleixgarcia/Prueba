d98f67c5f3fe6924cdf1ffe4f908c2a5c293c015
aleixgarcia <agarcia1@institutmvm.cat>
2021-12-05 13:39:41 +0100

    Dissenya un algorisme que llegeixi des del teclat dos valors enters n1 i n2, comprovi que amb n1 < n2 (i mostri un error si no és així), i finalment escrigui tots els nombres enters dins l’interval [n1, n2] en ordre creixent

--- main.py
+++ main.py
@@ -6,42 +6,19 @@
 
 def main():
 
-    reg=int(input("Quants valors vols introduir?"))
-    llista_codi = list()
-    llista_tlf = list()
-    llista_edat= list()
-    llista_cntcte= list()
+    num1=int(input("introdueix el primer num: "))
+    num2=int(input("introdueix el segon num: "))
+    aux=1
+
+    while num1>num2:
+        print("Error!")
+        num2=int(input("introdueix un numero major que el primer: "))
+    while num1<num2:
+        print(num1, end=",")
+        num1=aux+num1
+    print(num2,end="")
 
 
-    for x in range(reg):
-
-
-        nom=str(input("Introdueix el teu nom:"))
-        cognom1=str(input("Introdeuix el primer cognom:"))
-        cognom2=str(input("Introdueix el segon cognom:"))
-
-        llista_codi.append(cognom1[0:2]+cognom2[0:2]+nom[0:2])
-
-        tlf=int(input("Introdueix el telèfon de contacte:"))
-        l_tlf=len(str(tlf))
-        while l_tlf<9 or l_tlf>9:
-            tlf = int(input("Introdueix el telèfon de contacte de 9 nums:"))
-            l_tlf = len(str(tlf))
-        llista_tlf.append(tlf)
-
-        edat= int(input("introdueix l'edat de la persona: "))
-        while edat<=0:
-            edat= int(input("introdueix un altre edat de la persona: "))
-        llista_edat.append(edat)
-
-        llista_cntcte.append(bool(input("Si vols que tinguem el teu contacte escriu, sino dona-li al enter:")))
-
-    print("****************************************************")
-    print("Codi\t|\tTelèfon\t\t|\t\tEdat\t|\tContacte")
-    print("----------------------------------------------------")
-    for x in range(reg):
-        print(llista_codi[x], "\t|\t", llista_tlf[x], "\t|\t\t", llista_edat[x], "\t|\t", llista_cntcte[x])
-    print("****************************************************")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
