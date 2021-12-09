1fb46915901e155c95b04bc9e0e7f017a4b81bc8
aleixgarcia <agarcia1@institutmvm.cat>
2021-12-09 13:24:52 +0100

    Exercicis : UF1. A11

--- main.py
+++ main.py
@@ -14,6 +14,33 @@ def main():
    nums=[x for x in range(1,1001) if x % 8 == 0]
    print(nums)
 
+
+   # Exercici 3 --------------------------------
+   #nums=[x for x in range(1,1001)]
+   #nums_six=[x for x in nums if x in  ]
+   #print(nums_six)
+
+
+   # Exercici 4 --------------------------------
+   state = 'Practica els problemes de list comprehensions per a ser més Pythonic!'
+   v_state=[x.count(' ') for x in state if x in ' ']
+   print(v_state)
+
+
+   # Exercici 5 --------------------------------
+   state = 'Practica els problemes de list comprehensions per a ser més Pythonic!'
+   vowels=('a','e','i','o','u')
+   f_state=[x for x in state if x not in vowels]
+   print(f_state)
+
+
+   # Exercici 6 --------------------------------
+   state = 'Practica els problemes de list comprehensions per a ser més Pythonic!'
+   p_state=[x for x in state.split() if len(x) <5]
+   print(p_state)
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
