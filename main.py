7ea22f78b29215d6255d39cfc657a63b26855471
aleixgarcia <agarcia1@institutmvm.cat>
2021-12-09 12:25:59 +0100

    Dissenya un algorisme que mostri per pantalla una llista dels nombres parells entre el 0 i el 30 (inclosos).

--- main.py
+++ main.py
@@ -5,33 +5,9 @@
 
 def main():
 
-   dict={
-       "username":[],
-       "department":[],
-       "classroom":[]
-   }
 
-   num=int(input("Introdueix el num d'usuaris: "))
-
-   for x in range(num):
-       user=str(input("Introdueix el nom d'usuari: "))
-       dict["username"]=user
-
-       dpt=str(input("Introdueix el nom de departament: "))
-       dict["department"]=dpt
-
-       clas=int(input("Introdueix el numero de classe:"))
-       while clas<1 or clas>15:
-           clas= int(input("Introdueix un num de l'1 al 15:"))
-       dict["classroom"]=clas
-
-       for x in range(1):
-           print("*************************************************")
-           print("USUARI\t\t|\tDEPARTAMENT\t|\t\tCLASSE")
-           print("-------------------------------------------------")
-           print(dict["username"],dict["department"],dict["classroom"],sep="\t\t|\t\t")
-       print("*************************************************")
-       dict.clear()
+   nums=[x for x in range(0,31,2)]
+   print(nums)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
