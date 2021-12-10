562d24f30e87cfc177e7d82b6d10074e616db199
aleixgarcia <agarcia1@institutmvm.cat>
2021-12-10 11:54:40 +0100

    Exercicis : UF1. A11

--- main.py
+++ main.py
@@ -5,41 +5,33 @@
 
 def main():
 
-   #Exercici 1 --------------------------------
+   # Exercici 1 --------------------------------
    nums = [x for x in range(0, 31, 2)]
    print(nums)
 
-
-   #Exercici 2 --------------------------------
-   nums=[x for x in range(1,1001) if x % 8 == 0]
+   # Exercici 2 --------------------------------
+   nums = [x for x in range(1, 1001) if x % 8 == 0]
    print(nums)
 
-
    # Exercici 3 --------------------------------
-   nums=[x for x in range(1,1001)]
-   nums_six=[x for x in nums if x == 6 ]
-   print(nums_six)
-
+   nums = [x for x in range(1, 1001) if '6' in str(x)]
+   print(nums)
 
    # Exercici 4 --------------------------------
    state = 'Practica els problemes de list comprehensions per a ser més Pythonic!'
-   v_state=[x.count(' ') for x in state if x in ' ']
-   print("Hay",len(v_state),"espacios")
-
+   v_state = [x.count(' ') for x in state if x in ' ']
+   print("Hay", len(v_state), "espacios")
 
    # Exercici 5 --------------------------------
    state = 'Practica els problemes de list comprehensions per a ser més Pythonic!'
-   vowels=('a','e','i','o','u')
-   f_state=[x for x in state if x not in vowels]
+   vowels = ('a', 'e', 'i', 'o', 'u')
+   f_state = [x for x in state if x not in vowels]
    print(f_state)
-
 
    # Exercici 6 --------------------------------
    state = 'Practica els problemes de list comprehensions per a ser més Pythonic!'
-   p_state=[x for x in state.split() if len(x) <5]
+   p_state = [x for x in state.split() if len(x) < 6]
    print(p_state)
-
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
